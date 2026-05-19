2860ea7d5cfe94d9ddbf551c2e2def9d3ed3eec4
Armaan Gupta <151835988+armaanngupta@users.noreply.github.com>
2026-05-19 09:55:50 +0530

    made port to be picked by env

--- src/main/java/p2p/App.java
+++ src/main/java/p2p/App.java
@@ -5,9 +5,10 @@ import p2p.controller.FileController;
 public class App {
     public static void main(String[] args) {
        try{
-          FileController fileController = new FileController(8080);
+          int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+          FileController fileController = new FileController(port);
           fileController.start();
-          System.out.println("PeerLink server started at port 8080");
+          System.out.println("PeerLink server started at port " + port);
           System.out.println("UI available at http://localhost:3000");
           Runtime.getRuntime().addShutdownHook(
              new Thread(
